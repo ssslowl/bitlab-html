4438902fb4683874ce8432a91ab325b1fc224cb2
ssslowl <ssslowl2005@gmail.com>
2023-12-23 14:29:05 +0600

    test

--- js 5.8/js/script.js
+++ js 5.8/js/script.js
@@ -5,6 +5,7 @@ var cost = wiskas * 120;
 alert("main cost: " + cost);
 if(cost >= 4000){
     alert("You need to pay " + (cost*0.9));
+    alert("it worked")
 }else{
     alert("You need to pay " + cost);
 }
