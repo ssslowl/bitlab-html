db3f8e3383654ab3a5503c36f62309c8e95c061d
ssslowl <ssslowl2005@gmail.com>
2023-12-23 20:03:30 +0600

    7.2

---
+++ js 7.2/js/script.js
@@ -0,0 +1,18 @@
+const numbers = [];
+
+while (true) {
+    input = parseFloat(prompt("Введите число:"));
+    if (input == 0) {
+        break;
+    }else{
+    numbers.push(input);
+    }
+}
+
+let product = 1;
+for (let i = 0; i < numbers.length; i++) {
+    product *= numbers[i];
+}
+
+const geometricMean = Math.pow(product, 1 / numbers.length);
+console.log("Геометрическая средняя:", geometricMean);
