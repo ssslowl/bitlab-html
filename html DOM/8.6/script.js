a8bb9d6c71778eed952e376f7b65f83afead3c81
ssslowl <ssslowl2005@gmail.com>
2024-01-16 19:25:36 +0600

    task 3 Changes

---
+++ html DOM/8.6/script.js
@@ -0,0 +1,12 @@
+usernames = ['john', 'sarah', 'adam', 'oliver'];
+
+emails = {'john': 'john@gmail.com', 'sarah': 'sarah@gamil.com', 'adam': 'adam@hmail.com', 'oliver': 'oliver@gmail.com'};
+
+passwords = {'john': 'john123', 'sarah': 'sarah123', 'adam': 'adam123', 'oliver': 'oliver123'};
+
+
+
+username = document.getElementById("name");
+email = document.getElementById("email");
+password = document.getElementById("password");
+
